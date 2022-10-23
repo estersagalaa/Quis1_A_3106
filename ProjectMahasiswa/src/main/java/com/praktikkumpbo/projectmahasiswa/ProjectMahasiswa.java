8ebf999108261d1e8f950e6ce4600204ba73903d
Ester Sagala <estersagala18@gmail.com>
2022-10-23 23:29:16 +0700

    ester a sagala(21103106) laporan modul1(Mahasiswa)

---
+++ ProjectMahasiswa/src/main/java/com/praktikkumpbo/projectmahasiswa/ProjectMahasiswa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.praktikkumpbo.projectmahasiswa;
+
+/**
+ *
+ * @author IP130-14AST A4R3
+ */
+public class ProjectMahasiswa {
+
+    public static void main(String[] args) {
+       Mahasiswa M=new Mahasiswa(); //Pembentukan Objek
+       M.inputData(); // Pemanggilan method
+       M.display ();
+    }
+}
