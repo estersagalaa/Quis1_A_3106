89f79fe36bf1c5bec1ac880e25f71aeca7d67d51
Ester Sagala <estersagala18@gmail.com>
2022-11-14 22:55:46 +0700

    ester a sagala(21103106) modul4 Inheritance(Unguided)

---
+++ Asuransi/src/main/java/com/praktikkumpbo/asuransi/Pegawai.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.praktikkumpbo.asuransi;
+
+/**
+ *
+ * @author IP130-14AST A4R3
+ */
+public class Pegawai {
+    protected String NIP;
+    protected String nama;
+    protected String alamat;
+    protected int tahunMasuk;
+    protected int gajiAkhir;
+}
